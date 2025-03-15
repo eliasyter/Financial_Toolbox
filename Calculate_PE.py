91265d392aef312f7e66b05e10022ad60fcbc035
eliasyter <eliasyter@gmail.com>
2025-03-16 00:23:17 +0100

    making progress on the feature

--- Calculate_PE.py
+++ Calculate_PE.py
@@ -10,24 +10,59 @@
 
 # plot
 
-
+import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd 
 
 
 import yfinance as yf
 
-# Angi ticker-symbolet
-ticker_symbol = "AAPL"
+def calculate_PE(ticker_symbol="TSLA"):
+        
+    
 
-# Hent aksjeinformasjon
-stock = yf.Ticker(ticker_symbol)
+        
+    ticker = yf.Ticker(ticker_symbol)
 
-print(stock.income_stmt)
-print(stock.income_stmt["2024-09-30"]["Total Revenue"])
+    # Get the income statement
+    income_stmt = ticker.income_stmt
 
+    # Extract the timestamps (dates) into a list
+    timestamps = income_stmt.columns.tolist()
+    dates=[]
+    pe_date=[]
+    for rapport_date in timestamps:
+        stock_price = ticker.history(start=pd.Timestamp(rapport_date) - pd.Timedelta(days=7), 
+                                  end=pd.Timestamp(rapport_date) + pd.Timedelta(days=7))['Close']
+        temp_price=0
+        for i,price in enumerate(stock_price):
+            temp_price+=price
+        avrage_price= temp_price/(i+1)
+        profit=income_stmt[rapport_date]["Gross Profit"]
+        outstanding_shares=ticker.info.get('sharesOutstanding')
+        pe=PE(avrage_price,profit,outstanding_shares)
+        dates.append(rapport_date.year)
+        pe_date.append(pe)
 
+    plt.plot(dates, pe_date, marker='o', linestyle='-', color='b')
 
+    
+    plt.title(f"PE_Over_Time for {ticker_symbol}")
+    plt.xlabel("Year")
+    plt.ylabel("PE")
+
+   
+
+   
+    plt.show()
+        
+    
+
+    
+
+def PE(price:int, earnings_12months:int, amount_stocks: int)->int: 
+    pe = (price*amount_stocks)/earnings_12months
+    return pe
 
 
 
