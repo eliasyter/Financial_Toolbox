50438feb04087fb5f69f90ac5f080918b6a5e14e
vetlerl <rambergl@insa-toulouse.fr>
2025-03-19 15:41:43 +0100

    vi har legit en funksjon som gir finansielle raad

--- optimisation.py
+++ optimisation.py
@@ -18,6 +18,7 @@ def Mean_Variance(log_returns, mu_p):
     w     = lam*cov_1@r + gam*cov_1@np.ones(N) 
     return w
 
+
 def Mean_Variance_No_Constraints_Naive(a, r_p, period='1mo', interval='1d', start=None, end=None):
     #import Tickers object
     tickers = yf.Tickers(a)
@@ -63,20 +64,40 @@ def Mean_Variance_No_Shortselling(a, r_p, period='1mo', interval='1d', start=Non
     log_returns,names = get_data.Get_Normalized_Log_Returns(a,period=period,interval=interval,start=start,end=end)
     names = np.array(names)
     mu_p = np.log(r_p+1)
-    max_iter = log_returns.shape[1]
-    short_map = np.ones(log_returns.shape[1], dtype=bool)
+    max_iter = log_returns.shape[1] # we can maximum iterate N times
+    long_map = np.ones(log_returns.shape[1], dtype=bool) # to begin we consider every weight
+
+    N     = int(log_returns.shape[1])
+    cov   = np.cov(log_returns.T) * 252
+    cov_1 = npl.solve(cov,np.identity(N))
+    r     = np.mean(log_returns,axis=0) * 252
+    
+    print(f"r     shape = {r.shape}")
+    print(f"cov_1 shape = {cov_1.shape}")
     
     for i in range(max_iter):
-        log_returns_filtered = log_returns.iloc[:,short_map]
         
-        if log_returns_filtered.shape[1] == 0:
+        cov_1 = cov_1 * np.outer(long_map,long_map)
+        # recompute variables
+        A       = np.ones((1,N)) @ cov_1 @ r
+        B       = r.T @ cov_1 @ r
+        C       = np.ones((1,N)) @ cov_1 @ np.ones((N,1))
+        D       = B*C - A**2
+        if D == 0:
+            # something went wrong
             weights = np.array([])
-        elif log_returns_filtered.shape[1] == 1:
-            #cannot compute covariance matrix
-            None
-        weights = Mean_Variance(log_returns_filtered, mu_p)
-        short_map[short_map] = weights > 0
-        if short_map.sum()==0:
+            print("failed to find optimal pf")
             break
+        lam     = (C*mu_p - A)/D
+        gam     = (B - A*mu_p)/D
+        weights = lam*cov_1@r + gam*cov_1@np.ones(N) 
+
+        short_map = weights < 0
+        long_map  = weights >= 0
+
+        if short_map.sum()==0:
+            # every weight is non-negative
+            break
+        
     return weights,names
     
