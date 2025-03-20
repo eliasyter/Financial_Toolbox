22895e35a7135044f3165836ac7467756b328fbd
eliasyter <eliasyter@gmail.com>
2025-03-20 17:25:17 +0100

    creating the first classes

--- simulate.py
+++ simulate.py
@@ -1,13 +1,20 @@
 
 
+class Portfolio():
+    def __init__(self,tickers:list[str]):
+        stocks=[]
+        for ticker in tickers:
+            stocks.append(Stock(ticker))
 
 
 
 
 
 
-
-
+class Stock():
+    def __init__(self,ticker):
+        self.ticker=ticker
+        self.five_year_price=[]
 
 
 
